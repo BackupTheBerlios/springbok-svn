d1967d2c9eda1e957836d5dfc0afa0fb92e5b69c
hirantha <hirantha@366d0465-c784-0410-aa8f-f854f4e573d5>
2010-12-03 23:23:16 +0000

    Modified to store Query

--- SpringbokCore/trunk/src/model/DataModel.java
+++ SpringbokCore/trunk/src/model/DataModel.java
@@ -1,33 +1,88 @@
 package model;
 
+/**
+ * Data model to store query result
+ * 
+ * @author Hirantha Bandara
+ * 
+ */
 public class DataModel {
 
 	private String name;
 	private String[] columnNames;
 	private String[][] data;
-	
+	private String query;
+
+	/**
+	 * Returns table name
+	 * 
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets table name
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Returns column names
+	 * 
+	 * @return
+	 */
 	public String[] getColumnNames() {
 		return columnNames;
 	}
 
+	/**
+	 * Sets column names
+	 * 
+	 * @param columnNames
+	 */
 	public void setColumnNames(String[] columnNames) {
 		this.columnNames = columnNames;
 	}
 
+	/**
+	 * Returns query results
+	 * 
+	 * @return
+	 */
 	public String[][] getData() {
 		return data;
 	}
 
+	/**
+	 * Sets query results
+	 * 
+	 * @param data
+	 */
 	public void setData(String[][] data) {
 		this.data = data;
 	}
-	
+
+	/**
+	 * Returns the query
+	 * 
+	 * @return
+	 */
+	public String getQuery() {
+		return query;
+	}
+
+	/**
+	 * Sets query
+	 * 
+	 * @param query
+	 */
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
 }

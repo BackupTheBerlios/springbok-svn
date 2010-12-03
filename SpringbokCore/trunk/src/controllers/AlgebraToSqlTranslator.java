2346359220153a3a6dbd6851b2ac7c3e3a2f409e
hirantha <hirantha@366d0465-c784-0410-aa8f-f854f4e573d5>
2010-12-03 23:25:43 +0000

    Added support to translate where clause

--- SpringbokCore/trunk/src/controllers/AlgebraToSqlTranslator.java
+++ SpringbokCore/trunk/src/controllers/AlgebraToSqlTranslator.java
@@ -1,16 +1,49 @@
 package controllers;
 
+import utils.AlgebraConstants;
+import utils.StringUtils;
+
 public class AlgebraToSqlTranslator {
 
-	public String translate(String algebra) {
-		StringBuffer sql = new StringBuffer();
+	public String translate(String algebra) {		
+		String message = null;
 		
-		if(algebra != null && algebra.trim().length() != 0) {
-			algebra = algebra.replaceAll("\u03C3", "select * from");
-			sql.append(algebra);
+		if(StringUtils.isNotEmpty(algebra)) {
+			algebra = StringUtils.normalize(algebra);
+			char ch = algebra.charAt(0);
+			if(ch == AlgebraConstants.SELECT)
+				message = translateSelectStatement(algebra);
+		}		
+		return message;		
+	}
+
+	private String translateSelectStatement(String algebra) {
+				
+		int indexOfOpenBrace = algebra.indexOf('(');		
+		int indexOfCloseBrace = algebra.indexOf(')');
+		
+		String tableName = algebra.substring(indexOfOpenBrace+1, indexOfCloseBrace);
+		tableName = StringUtils.normalize(tableName);
+				
+		/*
+		 * translate whereClause (tableName);
+		 */
+		String whereClause = algebra.substring(1, indexOfOpenBrace);
+		whereClause = StringUtils.normalize(whereClause);
+		
+		if(StringUtils.isNotEmpty(whereClause)) {
+			String message = translateWhereClause(whereClause);
+			if(message != null)				
+				return "Select * from "+tableName+" " + message;			
 		}
 		
-		return sql.toString();
+		return "Select * from "+tableName;
+	}
+
+	private String translateWhereClause(String whereClause) {
+		if(StringUtils.isNotEmpty(whereClause))
+			return "where "+whereClause;
+		return null;
 	}
 	
 }
